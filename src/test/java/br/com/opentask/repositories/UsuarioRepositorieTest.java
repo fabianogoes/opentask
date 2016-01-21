2e23932dc025b32e0aad5dd9984c6f09e84daf2b
Fabiano Góes <fabianogoes@gmail.com>
2016-01-21 21:41:34 -0200

    implementei o pattern Builder para facilitar a Construção de objeto Usuario para teste e evitar duplicar código

--- src/test/java/br/com/opentask/repositories/UsuarioRepositorieTest.java
+++ src/test/java/br/com/opentask/repositories/UsuarioRepositorieTest.java
@@ -3,6 +3,7 @@ package br.com.opentask.repositories;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,25 +22,34 @@ public class UsuarioRepositorieTest {
 	@Autowired
 	private UsuarioRepositorie usuarioRepositorie; 
 	
+	@Before
+	public void before(){
+		usuarioRepositorie.save( new UsuarioBuilder().build() );
+	}
+	
 	@Test
 	public void testInsertUsuario(){
-		Usuario usuario = new Usuario();
-		usuario.setNome("Fabiano");
-		usuario.setLogin("fgoes");
-		usuario.setSenha("123");
+		// Fabiano
+//		Usuario usuario = new Usuario();
+//		usuario.setNome("Fabiano");
+//		usuario.setLogin("fgoes");
+//		usuario.setSenha("123");
+//		usuario = usuarioRepositorie.save(usuario);
 		
-		usuario = usuarioRepositorie.save(usuario);
+		Usuario usuario = usuarioRepositorie.save(new UsuarioBuilder().build());
 		Assert.assertTrue("o id deve ser maior que 0 depois de inserido", usuario.getId() > 0);
 	}
 	
 	@Test
 	public void testUpdateUsuario(){
-		Usuario usuario = new Usuario();
-		usuario.setNome("Fabiano");
-		usuario.setLogin("fgoes");
-		usuario.setSenha("123");
+		// FelixPinho
+//		Usuario usuario = new Usuario();
+//		usuario.setNome("Fabiano");
+//		usuario.setLogin("fgoes");
+//		usuario.setSenha("123");
+//		usuario = usuarioRepositorie.save(usuario);		
 		
-		usuario = usuarioRepositorie.save(usuario);
+		Usuario usuario = usuarioRepositorie.save( new UsuarioBuilder().build() );
 		usuario.setNome("FelixPinho");
 		usuario = usuarioRepositorie.save(usuario);
 		Assert.assertEquals("O Valor alterado do nome do usuário deve ser FelixPinho", "FelixPinho", usuario.getNome());
@@ -48,11 +58,14 @@ public class UsuarioRepositorieTest {
 	
 	@Test
 	public void testDeleteUsuario() {
-		Usuario usuario = new Usuario();
-		usuario.setLogin("ddamaceno");
-		usuario.setNome("Diego");
-		usuario.setSenha("123456");
-		usuario = usuarioRepositorie.save(usuario);
+		// Diego
+//		Usuario usuario = new Usuario();
+//		usuario.setLogin("ddamaceno");
+//		usuario.setNome("Diego");
+//		usuario.setSenha("123456");
+//		usuario = usuarioRepositorie.save(usuario);
+		
+		Usuario usuario = usuarioRepositorie.save( new UsuarioBuilder().build() );
 		Long id = usuario.getId();
 		usuarioRepositorie.delete(usuario);
 		usuario = usuarioRepositorie.findOne(id);
@@ -61,16 +74,15 @@ public class UsuarioRepositorieTest {
 	
 	@Test
 	public void testFindOne(){
+		// Klayton
+//		Usuario usuario = new Usuario();
+//		usuario.setLogin("ksouza");
+//		usuario.setNome("Klayton");
+//		usuario.setSenha("654321");
+//		usuario = usuarioRepositorie.save(usuario);
 		
-		Usuario usuario = new Usuario();
-		usuario.setLogin("ksouza");
-		usuario.setNome("Klayton");
-		usuario.setSenha("654321");
-		usuario = usuarioRepositorie.save(usuario);
-		
-		Long id = usuario.getId();
-		
-		usuario = usuarioRepositorie.findOne(id);
+		Long id = usuarioRepositorie.save( new UsuarioBuilder().build() ).getId();
+		Usuario usuario = usuarioRepositorie.findOne(id);
 		
 		Assert.assertNotNull("Objeto não pode ser nulla.:", usuario);
 		Assert.assertEquals("O usuario a ser buscado deve ser o Klayton", "Klayton", usuario.getNome());
@@ -78,15 +90,16 @@ public class UsuarioRepositorieTest {
 	
 	@Test
 	public void testFindAll(){
+		// Lucas
+//		List<Usuario> usuarios = null;
+//		Usuario usuario = new Usuario();
+//		usuario.setLogin("lmarchetto");
+//		usuario.setNome("Lucas");
+//		usuario.setSenha("654321");
+//		usuario = usuarioRepositorie.save(usuario);
 		
-		List<Usuario> usuarios = null;
-		Usuario usuario = new Usuario();
-		usuario.setLogin("lmarchetto");
-		usuario.setNome("Lucas");
-		usuario.setSenha("654321");
-		usuario = usuarioRepositorie.save(usuario);
-		
-		usuarios = (List<Usuario>) usuarioRepositorie.findAll();
+		usuarioRepositorie.save( new UsuarioBuilder().build() );
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepositorie.findAll();
 		
 		Assert.assertNotNull("Lista nao pode ser nula", usuarios );
 		Assert.assertTrue("A lista Deve ser  maior que 0", usuarios.size() > 0);
@@ -94,19 +107,45 @@ public class UsuarioRepositorieTest {
 	
 	@Test
 	public void testFindByNome(){
-		List<Usuario> usuarios = null;
-		Usuario usuario = new Usuario();
-		usuario.setLogin("fsantaniello");
-		usuario.setNome("Felipe Santaniello");
-		usuario.setSenha("2222");
+		// Felipe Santaniello
+//		List<Usuario> usuarios = null;
+//		Usuario usuario = new Usuario();
+//		usuario.setLogin("fsantaniello");
+//		usuario.setNome("Felipe Santaniello");
+//		usuario.setSenha("2222");
+//		usuario = usuarioRepositorie.save(usuario);
 		
-		usuario = usuarioRepositorie.save(usuario);
-		usuarios = usuarioRepositorie.findByNome(usuario.getNome());	
+		Usuario usuario = new UsuarioBuilder().build();
+		List<Usuario> usuarios = usuarioRepositorie.findByNome(usuario.getNome());	
 		
 		Assert.assertNotNull("Lista nao pode ser nula", usuarios );
 		Assert.assertTrue("A Lista deve ser maior que 0", usuarios.size() > 0);
 		
 		boolean exist = usuarios.contains( usuario );
 		Assert.assertTrue("Deve existir um Usuario com o Nome Felipe Santaniello", exist);
+	}
+}
+
+class UsuarioBuilder{
+	private Usuario usuario;
+
+	public UsuarioBuilder(){
+		this.usuario = new Usuario(null, "TESTE", "teste", "123");
+	}
+	public UsuarioBuilder comNome(String nome){
+		this.usuario.setNome(nome);
+		return this;
+	}
+	public UsuarioBuilder comLogin(String login){
+		this.usuario.setLogin(login);
+		return this;
+	}
+	public UsuarioBuilder comSenha(String senha){
+		this.usuario.setSenha(senha);
+		return this;
+	}
+	
+	public Usuario build(){
+		return this.usuario;
 	}
 }
